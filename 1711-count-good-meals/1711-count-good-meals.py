37deca37dd6de9df3810e116080edc2b281a1634
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2022-12-23 17:31:30 +0300

    Time: 2880 ms (56.72%), Space: 21.9 MB (55.04%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -3,11 +3,10 @@ class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
         ans = 0
         dictD = Counter(deliciousness)
-        powers = [i for i in range(22)]
-        # checked = set()
+        powers = [2**i for i in range(22)]
         for item in deliciousness:
             dictD[item] -= 1
             for power in powers:
-                temp = 2**power - item
-                ans += dictD.get(temp, 0)
+                adjacent = power - item
+                ans += dictD.get(adjacent, 0)
         return ans % (10**9 + 7)
