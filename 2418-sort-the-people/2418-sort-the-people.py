279e3cd678eda104547259099967207d83627e09
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-01-17 18:29:20 +0300

    Time: 2361 ms (5.03%), Space: 14.5 MB (42.89%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        for _ in range(len(heights)):
+            for i in range(1,len(heights)):
+                if heights[i-1] > heights[i]:
+                    heights[i-1], heights[i] = heights[i], heights[i-1]
+                    names[i-1], names[i] = names[i], names[i-1]
+        names.reverse()
+        return names
+                
