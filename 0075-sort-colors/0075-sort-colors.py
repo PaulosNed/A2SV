23e22860e2ff0350dab956ab16dc3eb9f830c0ce
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-01-19 16:25:37 +0300

    Time: 23 ms (61.88%), Space: 13.5 MB (14.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,14 +2,8 @@ class Solution(object):
     def sortColors(self, nums):
         if len(nums) < 2:
             return nums
-        i = 1
-        while(i<len(nums)):
-            if nums[i] < nums[i-1]:
-                j = i
-                while(nums[j] < nums[j-1]):
-                    nums[j], nums[j-1] = nums[j-1], nums[j]
-                    j-=1
-                    if (j <= 0):
-                        break
-            i+=1
+        for _ in range(len(nums)):
+            for i in range(1, len(nums)):
+                if nums[i] < nums[i-1]:
+                    nums[i], nums[i-1] = nums[i-1], nums[i]
         return nums
