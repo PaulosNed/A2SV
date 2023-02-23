e26120ce3c461133aa354be3ed3f69c997773f62
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-02-23 17:24:09 +0300

    Time: 375 ms (26.18%), Space: 15.3 MB (26.13%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,25 +1,19 @@
 class Solution(object):
     def findAnagrams(self, s, p):
-        p = list(p)
-        p.sort()
-        p = "".join(p)
         start = 0
-        end = start + len(p) - 1
-        s = list(s)
-        anagList = s[start: end + 1]
-        anagList.sort()
-        final = []
+        end = len(p)-1
+        ans = []
+        countWord = Counter(s[start:end+1])
+        countP = Counter(p)
         while(end < len(s)):
-            anag = "".join(anagList)
-            if (anag == p):
-                final.append(start)
-            anagList.remove(s[start])
+            if countWord == countP:
+                ans.append(start)
+            countWord[s[start]] -= 1
+            if countWord[s[start]] == 0:
+                countWord.pop(s[start])
             start += 1
             end += 1
-            if end >= len(s):
-                return final
-            bisect.insort(anagList, s[end])
-        return final
+            if end < len(s):
+                countWord[s[end]] = countWord.get(s[end], 0) + 1
+        return ans
                 
-            
-            
