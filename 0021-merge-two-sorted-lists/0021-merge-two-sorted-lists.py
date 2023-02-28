189b983fcc782851dca98baf50c1450fc751618d
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-02-28 18:02:23 +0300

    Time: 39 ms (65.72%), Space: 13.9 MB (23.48%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,36 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
+        if list1 == None:
             return list2
-        elif not list2:
+        if list2 == None:
             return list1
-        if(list1.val < list2.val):
-            head = list1
-            list1= list1.next
-        else:
-            head = list2
-            list2 = list2.next
+        
+        head = ListNode()
         final = head
-        while(list1 != None or list2 != None):
-            if list1 == None:
-                head.next = list2
-                head = head.next
-                list2 = list2.next
-            elif list2 == None:
-                head.next = list1
-                head = head.next
-                list1 = list1.next
-            elif list1.val <= list2.val:
-                head.next = list1
-                head = head.next
-                list1 = list1.next
-            else:
-                head.next = list2
-                head = head.next
-                list2 = list2.next
-        return final
+        
+        if list1.val <= list2.val:
+            head.next = list1
+            list1 = list1.next
+        else:
+            head.next = list2
+            list2 = list2.next
+        head = head.next
+            
+        nxt = self.mergeTwoLists(list1, list2)
+        head.next = nxt
+        return final.next
+    
+        
+        
