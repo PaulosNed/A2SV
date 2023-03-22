ad06e68185efb920b9df480bae4c5e7869e3a0e3
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-03-22 18:12:17 +0300

    Time: 2059 ms (30.24%), Space: 22.9 MB (36.25%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def merge(self, nums1: List[int], nums2: List[int]) -> None:
+        first = 0
+        second = 0
+        ans = []
+        while(first < len(nums1) and second < len(nums2)):
+            if nums1[first] <= nums2[second]:
+                ans.append(nums1[first])
+                first += 1
+            else:
+                ans.append(nums2[second])
+                second += 1
+        ans.extend(nums2[second:])
+        ans.extend(nums1[first:])
+        return ans
+                
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return self.mergeSort(0, len(nums)-1, nums)
+        
+    def mergeSort(self, left, right, arr):
+        if left == right:
+            return [arr[left]]
+        mid = left + (right - left) // 2
+        left_half = self.mergeSort(left, mid, arr)
+        right_half = self.mergeSort(mid + 1, right, arr)
+
+        return self.merge(left_half, right_half)
+
+        
