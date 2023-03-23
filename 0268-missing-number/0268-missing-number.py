a6c5c80be4ea01c0f89349ee51ba6cbe8141191b
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-03-23 21:01:20 +0300

    Time: 156 ms (29.33%), Space: 15.2 MB (29.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i,v in enumerate(nums):
-            if i != v:
+        nums.append(None)
+        for i in range(len(nums)):
+            while nums[i] != None and nums[i] != i:
+                idx = nums[i]
+                nums[i], nums[idx] = nums[idx], nums[i]
+        
+        for i, v in enumerate(nums):
+            if v == None:
                 return i
-        return len(nums)
+               
+        return -1
