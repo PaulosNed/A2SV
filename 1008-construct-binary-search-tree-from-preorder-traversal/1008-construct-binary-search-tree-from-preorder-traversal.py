eee8bf9ed80dafc5776781200c70cfb17a8c3a58
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-03-28 22:34:50 +0300

    Time: 36 ms (73.60%), Space: 14 MB (40.59%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -6,11 +6,33 @@
 #         self.right = right
 class Solution:
     def bstFromPreorder(self, A):
-        def helper(i, j):
-            if i == j: return None
-            root = TreeNode(A[i])
-            mid = bisect.bisect(A, A[i], i + 1, j)
-            root.left = helper(i + 1, mid)
-            root.right = helper(mid, j)
-            return root
-        return helper(0, len(A))
+        if len(A) == 1:
+            return TreeNode(A[0])
+        
+        stack = []
+        ans = None
+        last = None
+        for i, v in enumerate(A):
+            curr = TreeNode(v)
+            while stack and v > stack[-1].val:
+                popped = stack.pop()
+                last = popped
+                if popped.val == A[0]:
+                    ans = popped
+                if stack and not stack[-1].left:
+                    stack[-1].left = popped
+                if not stack or stack[-1].val > v:
+                    popped.right = curr
+                    
+            stack.append(curr)
+        
+        
+        if len(stack) > 1:
+            while len(stack) > 1:
+                popped = stack.pop()
+                if stack and not stack[-1].left:
+                    stack[-1].left = popped
+        
+        if not ans:
+            return stack[-1]
+        return ans
