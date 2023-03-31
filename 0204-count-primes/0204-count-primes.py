2d2b3b14a046a97341f17d0815a36612a468f19c
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-03-31 19:50:37 +0300

    Time: 5368 ms (26.19%), Space: 67.7 MB (35.95%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -8,16 +8,16 @@ class Solution:
         
         for i in range(3,n):
             if i % 2 == 0:
-                is_prime[i] = False
+                is_prime[i] = 0
 
-        i = 2
+        i = 1
 
         while i * i <= n:
             if is_prime[i]:
                 j = i * i
                 while j < n:
-                    is_prime[j] = False
+                    is_prime[j] = 0
                     j += i
-            i += 1
+            i += 2
         
         return sum(is_prime)
