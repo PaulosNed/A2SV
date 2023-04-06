978fb05a6dc67194c826e34963b719eac0818843
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-04-06 21:50:09 +0300

    Time: 67 ms (47.41%), Space: 15.6 MB (81.30%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,6 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        for k in count:
-            if count[k] == 1:
-                return k
+        nums.sort()
+        if len(nums) == 1:
+            return nums[0]
+        idx = 2
+        while(idx < len(nums)):
+            curr = nums[idx] ^ nums[idx-1] ^ nums[idx-2]
+            if curr != nums[idx]:
+                return curr
+            idx += 3
+        return nums[-1]
