f516d9e52609ce9894351259c0d73f8f83257211
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-05-19 18:40:51 +0300

    Time: 1815 ms (88.65%), Space: 104.9 MB (88.08%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -15,10 +15,10 @@ class Solution:
             
             if rank[parent1] >= rank[parent2]:
                 parent[parent2] = parent1
-                # parent[d] = parent1
+                rank[parent1] += rank[parent2]
             else:
                 parent[parent1] = parent2
-                # parent[s] = parent2
+                rank[parent2] += rank[parent1]
         
         return find(source) == find(destination)
             
