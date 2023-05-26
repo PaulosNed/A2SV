c6f5d48cdd7abb020a43aed01e81954af9d21816
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-05-26 19:43:24 +0300

    Time: 47 ms (29.97%), Space: 16.5 MB (13.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,26 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
         
-        if len(nums) == 2:
-            return max(nums)
-        
-        def dp(curr, memo):
-            if curr <= 1:
-                return nums[curr]
-
-            if curr == 2:
-                return nums[curr] + nums[0]
-
+        def dp(curr):
+            if curr >= len(nums) - 2:
+                return max(nums[curr:])
+            
             if curr not in memo:
-                memo[curr] = nums[curr] + max(dp(curr - 2, memo) , dp(curr - 3, memo))     
+                opt1 = dp(curr + 1)
+                opt2 = nums[curr] + dp(curr + 2)
+                memo[curr] = max(opt1, opt2)
             
             return memo[curr]
         
-        memo = defaultdict(int)
-        max_1 = dp(len(nums) - 1, memo)
-        max_2 = dp(len(nums) - 2, memo)
-        
-        return max(max_1, max_2)
-        
+        memo = {}
+        return dp(0)
