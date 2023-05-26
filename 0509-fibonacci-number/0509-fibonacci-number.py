d062d1bf3a17af3b13c8208c4eb72fb24f6eeae3
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-05-26 17:53:54 +0300

    Time: 50 ms (41.55%), Space: 16.4 MB (5.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,8 +3,11 @@ class Solution(object):
         if (n == 1):
             return 1
         
-        elif(n == 0):
+        if (n == 0):
             return 0
+        
+        if n in memo:
+            return memo[n]
         
         prev1 = self.fib(n-1, memo)
         prev2 = self.fib(n-2, memo)
