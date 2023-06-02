a29dc5aefa745c9859a581147ae93f694bfa87d2
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-06-02 19:10:19 +0300

    Time: 1259 ms (15.30%), Space: 32.5 MB (55.22%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,27 +1,17 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        stack = []
-        final = [None] * len(nums)
-        final2 = [None] * len(nums)
+        stack = [float('inf')] * len(nums)
+        prefix = nums[0]
         
-        for idx, val in enumerate(nums):
-            while stack and stack[-1][1] < val:
-                i, v = stack.pop()
-                final[i] = idx
-            
-            stack.append([idx, val])
+        for i in range(len(nums)):
+            prefix = min(prefix, nums[i])
+            stack[i] = prefix
         
-        stack = []
-        for idx in range(len(nums)-1, -1, -1):
-            while stack and nums[idx] < stack[-1][1]:
-                i, v = stack.pop()
-                final2[i] = idx
-            
-            stack.append([idx, nums[idx]])
-        
-        # print(final, final2)
-        for i in range(len(final)):
-            if final[i] != None and final2[i] != None:
+        prefix = float('-inf')
+        for i in range(len(nums) - 1, -1, -1):
+            prefix = max(nums[i], prefix)
+            if nums[i] != stack[i] and nums[i] != prefix:
                 return True
+        
         return False
                 
