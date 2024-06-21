0c36e7bd259bf72a532cd69f2b940c6e31ce2b28
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2024-06-21 15:36:14 +0300

    Time: 192 ms (68.49%), Space: 18.5 MB (71.23%) - LeetHub

--- 1210-minimum-moves-to-reach-target-with-rotations/1210-minimum-moves-to-reach-target-with-rotations.py
+++ 1210-minimum-moves-to-reach-target-with-rotations/1210-minimum-moves-to-reach-target-with-rotations.py
@@ -3,17 +3,29 @@ class Solution:
         n = len(grid)
         dq = collections.deque([((0, 0, 0, 1), 0)])
         visited = set()
+        
         while dq:
+            
             (tail_r, tail_c, head_r, head_c), step = dq.popleft()
-            if (tail_r, tail_c, head_r, head_c) in visited: continue
-            if (tail_r, tail_c) == (n - 1, n - 2) and (head_r, head_c) == (n - 1, n - 1): return step
+            
+            if (tail_r, tail_c, head_r, head_c) in visited: 
+                continue
+            
+            if (tail_r, tail_c) == (n - 1, n - 2) and (head_r, head_c) == (n - 1, n - 1): 
+                return step
+            
             visited.add((tail_r, tail_c, head_r, head_c))
             if head_c + 1 < n and grid[tail_r][tail_c + 1] != 1 and grid[head_r][head_c + 1] != 1: 
                 dq.append(((tail_r, tail_c + 1, head_r, head_c + 1), step + 1)) # move right
+            
             if head_r + 1 < n and grid[tail_r + 1][tail_c] != 1 and grid[head_r + 1][head_c] != 1: 
                 dq.append(((tail_r + 1, tail_c, head_r + 1, head_c), step + 1)) # move down
+            
             if tail_r == head_r and head_r + 1 < n and grid[tail_r + 1][tail_c] != 1 and grid[head_r + 1][head_c] != 1: 
                 dq.append(((tail_r, tail_c, head_r + 1, head_c - 1), step + 1)) # rotate clockwise
+            
             if tail_c == head_c and head_c + 1 < n and grid[tail_r][tail_c + 1] != 1 and grid[head_r][head_c + 1] != 1: 
                 dq.append(((tail_r, tail_c, head_r - 1, head_c + 1), step + 1)) # rotate counter clockwise
+                
+                
         return -1
